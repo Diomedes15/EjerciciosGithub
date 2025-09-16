169cc872c158684056f74faaff593825d2c42e30
Diomedes <wberf24@gmail.com>
2025-09-16 19:13:01 +0200

    cambio

--- ejercicio10.ts
+++ ejercicio10.ts
@@ -1,6 +1,6 @@
 let numero: number = 10;
 
-function calcularCubo (valor: number): number{
+function calcularCubo (valor: number): void{
     let numeroCalculado: number = Math.pow(valor, 3)
     console.log(numeroCalculado)
 }
