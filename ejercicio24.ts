79b8f61be25c0e84fec3bd448a2fbf7bd759288f
Diomedes <wberf24@gmail.com>
2025-09-19 15:13:33 +0200

    cambio

--- ejercicio24.ts
+++ ejercicio24.ts
@@ -7,14 +7,18 @@ function compararCadenas (cad1: string, cad2: string, cad3: string): string {
     const longitud2: number = cad2.length;
     const longitud3: number = cad3.length;
     if (longitud1 > longitud2 && longitud1 > longitud3){
-        return cad1;
+        return `La cadena de texto más larga es: ${cad1}`;
     } else if (longitud2 > longitud1 && longitud2 > longitud3){
-        return cad2;
+        return `La cadena de texto más larga es: ${cad2}`;
     } else if (longitud3 > longitud1 && longitud3 > longitud2){
-        return cad3;
+        return `La cadena de texto más larga es: ${cad3}`;
+    } else if (longitud1 == longitud2 && longitud1 > longitud3){
+        return `Las cadenas de texto más largas son: ${cad1} y ${cad2}`;
+    }else if (longitud2 == longitud3 && longitud2 > longitud1){
+        return `Las cadenas de texto más largas son: ${cad2} y ${cad3}`;
     }
 
-    return `Hay dos o más frases de la misma longitud`;
+    return `Las tres cadenas de texto son iguales`;
 }
 
 console.log(compararCadenas(cadena1, cadena2, cadena3))
