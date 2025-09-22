96d21641bb5e57a3a6bface2d927740495cab2a6
Diomedes <wberf24@gmail.com>
2025-09-22 19:23:33 +0200

    no

---
+++ ejercicio27.ts
@@ -0,0 +1,7 @@
+let palabra: string = "";
+let letra: string = "";
+
+function encontrarLetra (texto: string, coincidencia: string){
+    const separar: 
+    const s: boolean = texto.some(coincidencia)
+}
