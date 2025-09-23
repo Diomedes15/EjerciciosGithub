f6e5b8e1f923f470667c10ec358081328f03b365
Diomedes <wberf24@gmail.com>
2025-09-23 10:55:32 +0200

    hecho

--- ejercicio27.ts
+++ ejercicio27.ts
@@ -1,7 +1,15 @@
-let palabra: string = "";
-let letra: string = "";
+let palabra: string = "Antílope";
+let letra: string = "a";
 
-function encontrarLetra (texto: string, coincidencia: string){
-    const separar: 
-    const s: boolean = texto.some(coincidencia)
+function encontrarLetra (texto: string, coincidencia: string): string { 
+    const palabraMinus: string = texto.toLowerCase()
+    const letraMinus: string = coincidencia.toLowerCase()
+    const siono: boolean = palabraMinus.includes(letraMinus);
+    if (siono == true){
+        return `La palabra ${texto} tiene la letra ${coincidencia}`;
+    } else {
+        return `La palabra ${texto} no tiene la letra ${coincidencia}`;
+    }
 }
+
+console.log(encontrarLetra(palabra, letra));
